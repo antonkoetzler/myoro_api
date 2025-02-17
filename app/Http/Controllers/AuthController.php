f485c7184cee72915117aabfe63aa4127278ae00
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-02-17 16:15:00 -0300

    Signup and login routes created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function signup(Request $request): JsonResponse
     {
         $validated = $request->validate([
-            'username' => ['required', 'string', 'max:255', 'unique:users,username'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            User::NAME => ['required', 'string', 'max:255', 'unique:users,username'],
+            User::USERNAME => ['required', 'string', 'max:255', 'unique:users,username'],
+            User::EMAIL => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            User::PASSWORD => ['required', 'string', 'min:8', 'confirmed'],
         ]);
 
-        return response()->json(['message' => 'Validation passed', 'data' => $validated]);
+        // Create the user if they don't already exist in the DB.
+        $user = User::create([
+            User::NAME => $validated[User::NAME],
+            User::USERNAME => $validated[User::USERNAME],
+            User::EMAIL => $validated[User::EMAIL],
+            User::PASSWORD => bcrypt($validated[User::PASSWORD]),
+        ]);
+
+        // Return success message + newly created user's ID.
+        return response()->json([
+            'message' => 'User registered successfully!',
+            'user_id'    => $user->id,
+        ], 201);
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            User::USERNAME => ['nullable', 'string'],
+            User::EMAIL => ['nullable', 'string', 'email'],
+            User::PASSWORD => ['required', 'string'],
+        ]);
+
+        // Validation: Username (x)or email must be provided.
+        if (
+            (!isset($validated[User::USERNAME]) && !isset($validated[User::EMAIL])) ||
+            (isset($validated[User::USERNAME]) && isset($validated[User::EMAIL]))
+        ) {
+            return response()->json([
+                'message' => 'Username (x)or e-mail is required.',
+            ], 400);
+        }
+
+        // Column we are searching for the user with (either username or e-mail).
+        $user_identifier = $validated[User::USERNAME] != null ? User::USERNAME : ($validated[User::EMAIL] ? User::EMAIL : null);
+        assert($user_identifier != null);
+        $user = User::where($user_identifier, $validated[$user_identifier])->first();
+
+        // Password verification.
+        if (!$user || !Hash::check($validated[User::PASSWORD], $user->password)) {
+            return response()->json([
+                'message' => 'Username/email and/or password are incorrect.',
+            ], 401);
+        }
+
+        // Issue token.
+        $token = $user->createToken('MyoroAPI')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful!',
+            'user_id' => $user->id,
+            'token' => $token,
+        ], 200);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
+/**
+ *
+ *
+ * @property-read \App\Models\TFactory|null $use_factory
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
+    const NAME = 'name';
+    const USERNAME = 'username';
+    const EMAIL = 'email';
+    const PASSWORD = 'password';
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +36,10 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        User::NAME,
+        User::USERNAME,
+        User::EMAIL,
+        User::PASSWORD,
     ];
 
     /**
@@ -29,7 +48,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $hidden = [
-        'password',
+        User::PASSWORD,
         'remember_token',
     ];
 
@@ -42,7 +61,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            User::PASSWORD => 'hashed',
         ];
     }
 }
